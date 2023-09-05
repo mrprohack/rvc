8f3a5d0629777f934457355bd4fd18ab41c794f6
mrprohack <35624034+mrprohack@users.noreply.github.com>
2023-09-05 20:26:10 +0530

    Update EasierGUI.py

--- EasierGUI.py
+++ EasierGUI.py
@@ -4,7 +4,7 @@ os.environ["no_proxy"] = "localhost, 127.0.0.1, ::1"
 import threading
 from time import sleep
 from subprocess import Popen
-import faiss-gpu
+import faiss
 from random import shuffle
 import json, datetime, requests
 from gtts import gTTS
